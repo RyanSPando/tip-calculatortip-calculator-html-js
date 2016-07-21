df8a2cdd50c16df9c33fe15e9e386f60c6393dc2
Ryan "archaen" Pando <vretha@gmail.com>
2016-07-21 17:46:17 -0600

    Error handling implemented and only single error statement allowed in dom.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 $(function() {
-  'use strict';
+
   console.log('sanity check');
 
+  $('form').on('submit', function(event) {//event handler grabs total from form
+    event.preventDefault();
+
+    var userInput = ($('#total').val() || 0);
+
+    if ( isNaN(userInput) || parseInt(userInput) < 0) {
+      displayResults('<h1>This is not a valid input</h1>');
+    }
+    else {
+      displayResults('<h1> You should tip $' + tipCalculation(parseInt(userInput), 20) + '.</h1>');
+      // $('#save').append('<h1> You should tip $' + tipCalculation(parseInt(userInput), 20) + '.</h1>');
+    }
+
+  })
+
 });
+
+function displayResults(h1Element) {//
+  clearElement();
+  $('#save').append(h1Element);
+}
+
+function tipCalculation(totalCheck, percentage) {
+  return (totalCheck * percentage / 100).toFixed(2);
+}
+
+function clearElement(){
+  $('#save > h1').remove()
+}
