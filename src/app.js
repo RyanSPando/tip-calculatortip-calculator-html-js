a3aaaddc2dbc82296f97f26b7d085732cd149858
Ryan "archaen" Pando <vretha@gmail.com>
2016-07-21 18:18:23 -0600

    String parsing for inputted value implementation.

--- src/app.js
+++ src/app.js
@@ -2,32 +2,29 @@ $(function() {
 
   console.log('sanity check');
 
-  $('form').on('submit', function(event) {//event handler grabs total from form
+  $('form').on('submit', function(event) {
     event.preventDefault();
 
-    var userInput = ($('#total').val() || 0);
+    var userInput = parseFloat(($('#total').val() || 0).replace(/[^0-9.]/g, ''));//Grab value from form, if no value, 0.  Replace all non digit charcacters and periods with empty string and then parseFloat the value.
 
-    if ( isNaN(userInput) || parseInt(userInput) < 0) {
+    if (isNaN(userInput) || userInput < 0) {//check for a positive number, return error if not
       displayResults('<h1>This is not a valid input</h1>');
     }
-    else {
-      displayResults('<h1> You should tip $' + tipCalculation(parseInt(userInput), 20) + '.</h1>');
-      // $('#save').append('<h1> You should tip $' + tipCalculation(parseInt(userInput), 20) + '.</h1>');
+    else {//output tip amount
+      displayResults('<h1> You should tip $' + tipCalculation(userInput, 20) + '.</h1>');
     }
-
-  })
-
+  });
 });
 
-function displayResults(h1Element) {//
+function displayResults(h1Element) {//clear h1 element then add new one
   clearElement();
   $('#save').append(h1Element);
 }
 
-function tipCalculation(totalCheck, percentage) {
-  return (totalCheck * percentage / 100).toFixed(2);
+function tipCalculation(totalCheck, percentage) {//calculate tip and return value
+  return ((totalCheck * percentage / 100).toFixed(2));
 }
 
-function clearElement(){
-  $('#save > h1').remove()
+function clearElement() {//remove h1 element in div save
+  $('#save > h1').remove();
 }
