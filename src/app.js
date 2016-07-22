21ea82be59e7d6339e8207db7f2ffd3f04476613
Ryan "archaen" Pando <vretha@gmail.com>
2016-07-21 20:44:42 -0600

    Dropdown box functionality complete.

--- src/app.js
+++ src/app.js
@@ -1,30 +1,41 @@
 $(function() {
 
-  console.log('sanity check');
+  //assume 20% tip with no user input
+  var tipPercentage = 20;
 
+  //on click set tip percentage on dropdown
+  $('#dropdown li input').on('click', function() {
+    tipPercentage = parseFloat($(this).val().replace('%', ''));
+  });
+
+  //grab submit event
   $('form').on('submit', function(event) {
     event.preventDefault();
 
-    var userInput = parseFloat(($('#total').val() || 0).replace(/[^0-9.]/g, ''));//Grab value from form, if no value, 0.  Replace all non digit charcacters and periods with empty string and then parseFloat the value.
+    //Grab value from form, if no value, 0.  Replace all non digit characters and periods with empty string and then parseFloat the value.
 
-    if (isNaN(userInput) || userInput < 0) {//check for a positive number, return error if not
+    var userInput = parseFloat(($('#total').val()).replace(/[^0-9.]/g, ''));
+
+    //check for a positive number, return error if not.
+    if (isNaN(userInput) || userInput < 0) {
       displayResults('<h1>This is not a valid input</h1>');
     }
-    else {//output tip amount
-      displayResults('<h1> You should tip $' + tipCalculation(userInput, 20) + '.</h1>');
+    //output tip amount.
+    else {
+      displayResults('<h1> You should tip $' + tipCalculation(userInput, tipPercentage) + '.</h1>');
     }
   });
 });
 
-function displayResults(h1Element) {//clear h1 element then add new one
+function displayResults(h1Element) {//clear h1 element then add new one.
   clearElement();
   $('#save').append(h1Element);
 }
 
-function tipCalculation(totalCheck, percentage) {//calculate tip and return value
+function tipCalculation(totalCheck, percentage) {//calculate tip and return. value
   return ((totalCheck * percentage / 100).toFixed(2));
 }
 
-function clearElement() {//remove h1 element in div save
+function clearElement() {//remove h1 element in div save.
   $('#save > h1').remove();
 }
